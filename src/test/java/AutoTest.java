edf29a5961d6b1ef46f60fe3c29b7f01cdfc7f9e
orino1997 <40629102+orino1997@users.noreply.github.com>
2020-01-26 20:01:21 +0300

    supports different browser types, parameterized test included

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -1,31 +1,48 @@
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.junit.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
 public class AutoTest {
-    static ChromeDriver driver;
+    private WebDriver driver;
+    String browserType;
 
-    @BeforeClass
-    public static void testInitialize() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        driver = new ChromeDriver();
+    @BeforeEach
+    public void testInitialize() {
+        browserType = System.getenv("selenium.browser.type");
+        if (browserType == null) {
+            browserType = "chrome";
+        }
+        if (browserType.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else if (browserType.equalsIgnoreCase("internet explorer")) {
+            System.setProperty("webdriver.ie.driver", "drivers/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+        } else {
+            System.out.println("Not supported browser type");
+        }
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
-    @Test
-    public void test() {
+    @ParameterizedTest
+    @CsvSource( {"Ivanov, Ivan, 30012008", "Petrov, Petr, 20051997", "Sidorova, Anna, 2007"})
+    public void test(String surname, String name, String birthDate) {
         driver.navigate().to("http://www.sberbank.ru/ru/person");
         WebDriverWait wait = new WebDriverWait(driver, 30);
         driver.findElement(By.xpath("//span[.='Страхование']"));
@@ -38,9 +55,6 @@ public class AutoTest {
                 moveToElement(driver.findElement(By.xpath("//li[@class ='lg-menu__sub-item']//a[.='Страхование путешественников']"))).perform();
         driver.findElement(By.xpath("//li[@class ='lg-menu__sub-item']//a[.='Страхование путешественников']")).click();
 
-
-        //переход на страничку страховки для путешественников вручную
-        //driver.navigate().to("https://www.sberbank.ru/ru/person/bank_inshure/insuranceprogram/life/travel");
         if (driver.findElement(By.xpath("//div[@class='cookie-warning cookie-warning_show']")).isDisplayed()) {
             driver.findElement(By.xpath("//a[@class='cookie-warning__close']")).click();
         }
@@ -54,87 +68,12 @@ public class AutoTest {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
         button.click();
 
-        String[] insuredPersonSurname = {"Petrov", "Sidorov", "Kotova"};
-        String[] insuredPersonName = {"Ivan", "Igor", "Anna"};
-        String[] insuredPersonDate = {"01052001", "20101997", "28031992"};
-        WebElement surnameInput = driver.findElement
-                (By.xpath("//input[@id='surname_vzr_ins_0']"));
-        WebElement nameInput = driver.findElement
-                (By.xpath("//input[@id='name_vzr_ins_0']"));
-        WebElement birthDateInput = driver.findElement
-                (By.xpath("//input[@id='birthDate_vzr_ins_0']"));
-        for(int i = 0; i < insuredPersonSurname.length; i++) {
-            surnameInput.click();
-            surnameInput.sendKeys(insuredPersonSurname[i]);
-            nameInput.click();
-
-            nameInput.sendKeys(insuredPersonName[i]);
-            birthDateInput.click();
-
-            birthDateInput.sendKeys(insuredPersonDate[i]);
-            birthDateInput.sendKeys(Keys.RETURN);
-            WebElement title = driver.findElement(By.xpath("//legend[contains(text(), 'Страхователь')]"));
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", title);
-
-            if(i==0) {
-                WebElement surname = driver.findElement(By.xpath("//input[@id='person_lastName']"));
-                surname.click();
-                surname.sendKeys("Иванов");
-                //Assert.assertEquals(surname.getAttribute("placeholder"),"Иванов");
-
-                WebElement name = driver.findElement(By.xpath("//input[@id='person_firstName']"));
-                name.click();
-                name.sendKeys("Иван");
-
-                WebElement middleName = driver.findElement(By.xpath("//input[@id='person_middleName']"));
-                middleName.click();
-                middleName.sendKeys("Иванов");
-
-                WebElement date = driver.findElement(By.xpath("//input[@id='person_birthDate']"));
-                date.click();
-                date.sendKeys("01051997");
-
-                title = driver.findElement(By.xpath("//legend[contains(text(), 'Паспортные данные')]"));
-                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", title);
-
-                WebElement pasSeries = driver.findElement(By.xpath("//input[@id='passportSeries']"));
-                pasSeries.click();
-                pasSeries.sendKeys("2017");
-
-                WebElement pasNumber = driver.findElement(By.xpath("//input[@id='passportNumber']"));
-                pasNumber.click();
-                pasNumber.sendKeys("313234");
-
-                WebElement pasDate = driver.findElement(By.xpath("//input[@id='documentDate']"));
-                pasDate.click();
-                pasDate.sendKeys("17052017");
-
-                WebElement ufms = driver.findElement(By.xpath("//input[@id='documentIssue']"));
-                ufms.click();
-                ufms.sendKeys("УФМС");
-
-                button = driver.findElement(By.xpath("//button[@class='btn btn-primary page__btn waves-effect']"));
-                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
-                button.click();
-
-                Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert-form alert-form-error']")).isDisplayed());
-            }
-            if(i < insuredPersonSurname.length-1) {
-                surnameInput.clear();
-                nameInput.clear();
-                birthDateInput.clear();
-            }
-        }
-    }
-/*  @ParameterizedTest
-    @CsvSource( {"Ivanov, Ivan, 30012008", "Petrov, Petr, 20051997", "Sidorova, Anna, 2007"})
-    public void paramTestInsured(String surname, String name, String birthDate) {
-        WebElement surnameInput = driver.findElement
-                (By.xpath("//input[@id='surname_vzr_ins_0']"));
-        WebElement nameInput = driver.findElement
-                (By.xpath("//input[@id='name_vzr_ins_0']"));
-        WebElement birthDateInput = driver.findElement
-                (By.xpath("//input[@id='birthDate_vzr_ins_0']"));
+        WebElement surnameInput = driver
+                .findElement(By.xpath("//input[@id='surname_vzr_ins_0']"));
+        WebElement nameInput = driver
+                .findElement(By.xpath("//input[@id='name_vzr_ins_0']"));
+        WebElement birthDateInput = driver
+                .findElement(By.xpath("//input[@id='birthDate_vzr_ins_0']"));
         surnameInput.click();
         surnameInput.sendKeys(surname);
         nameInput.click();
@@ -142,11 +81,63 @@ public class AutoTest {
         birthDateInput.click();
         birthDateInput.sendKeys(birthDate);
         birthDateInput.sendKeys(Keys.RETURN);
-    }*/
 
-    @AfterClass
-    public static void endTest(){
-        driver.close();
-        driver.quit();
+        WebElement title = driver.findElement(By.xpath("//legend[contains(text(), 'Страхователь')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", title);
+
+        WebElement surnameIns = driver.findElement(By.xpath("//input[@id='person_lastName']"));
+        surnameIns.click();
+        surnameIns.sendKeys("Иванов");
+        Assert.assertEquals(surnameIns.getAttribute("value"),"Иванов");
+
+        WebElement nameIns = driver.findElement(By.xpath("//input[@id='person_firstName']"));
+        nameIns.click();
+        nameIns.sendKeys("Иван");
+        Assert.assertEquals(nameIns.getAttribute("value"),"Иван");
+
+        WebElement middleName = driver.findElement(By.xpath("//input[@id='person_middleName']"));
+        middleName.click();
+        middleName.sendKeys("Иванович");
+        Assert.assertEquals(middleName.getAttribute("value"),"Иванович");
+
+        WebElement date = driver.findElement(By.xpath("//input[@id='person_birthDate']"));
+        date.click();
+        date.sendKeys("01051997");
+        Assert.assertEquals(date.getAttribute("value"),"01.05.1997");
+
+        title = driver.findElement(By.xpath("//legend[contains(text(), 'Паспортные данные')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", title);
+
+        WebElement pasSeries = driver.findElement(By.xpath("//input[@id='passportSeries']"));
+        pasSeries.click();
+        pasSeries.sendKeys("2017");
+        Assert.assertEquals(pasSeries.getAttribute("value"),"2017");
+
+        WebElement pasNumber = driver.findElement(By.xpath("//input[@id='passportNumber']"));
+        pasNumber.click();
+        pasNumber.sendKeys("313234");
+        Assert.assertEquals(pasNumber.getAttribute("value"),"313234");
+
+        WebElement pasDate = driver.findElement(By.xpath("//input[@id='documentDate']"));
+        pasDate.click();
+        pasDate.sendKeys("17052017");
+        Assert.assertEquals(pasDate.getAttribute("value"),"17.05.2017");
+
+        WebElement ufms = driver.findElement(By.xpath("//input[@id='documentIssue']"));
+        ufms.click();
+        ufms.sendKeys("УФМС");
+        Assert.assertEquals(ufms.getAttribute("value"),"УФМС");
+
+        button = driver.findElement(By.xpath("//button[@class='btn btn-primary page__btn waves-effect']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
+        button.click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert-form alert-form-error']")).isDisplayed());
+    }
+
+    @AfterEach
+    public void endTest(){
+        this.driver.close();
+        this.driver.quit();
     }
 }
